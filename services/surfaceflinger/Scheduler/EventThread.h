e78b2ba06df610e90ce3b9f8868fcc68b1d23c0d
Dennis Kiilerich <dki@google.com>
2025-03-20 23:43:06 -0700

    Add tracing for EventThread VSYNC state
    This enables seeing whether VSYNC is synthetic, idle, or real VSYNC in Perfetto.
    
    Bug: 342681202
    
    Bug: 404073995
    
    Flag: EXEMPT tracing only update
    
    Test: verified in Perfetto with and without Android Auto Projected
    Change-Id: Id303ec71155c61bd67aadd627aab5438ef5fdec8

--- services/surfaceflinger/Scheduler/EventThread.h
+++ services/surfaceflinger/Scheduler/EventThread.h
@@ -225,6 +225,7 @@ private:
             std::shared_ptr<scheduler::VsyncSchedule>) EXCLUDES(mMutex);
 
     const char* const mThreadName;
+    std::string mEventThreadStateName;
     TracedOrdinal<int> mVsyncTracer;
     TracedOrdinal<std::chrono::nanoseconds> mWorkDuration GUARDED_BY(mMutex);
     std::chrono::nanoseconds mReadyDuration GUARDED_BY(mMutex);
@@ -268,6 +269,8 @@ private:
     };
 
     State mState GUARDED_BY(mMutex) = State::Idle;
+
+    void updateState(State state) REQUIRES(mMutex);
 
     static const char* toCString(State);
 };
