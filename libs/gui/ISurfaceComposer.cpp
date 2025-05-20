300046a3839ecbdbb7e4fee435028fac11c37a21
Surbhi Kadam <surbhikadam@google.com>
2025-05-20 15:21:30 -0700

    Don't set capacity on state vector when reading early wake up info.
    This was not cleaned up correctly when we changed earlyWakeupInfos to std::vector
    
    Bug: 419088315
    Flag: EXEMPT; refactor
    Test: presubmit
    
    Change-Id: Ia9fb8aac4f776fe80dd5ef7852f9d626b963b5c4

--- libs/gui/ISurfaceComposer.cpp
+++ libs/gui/ISurfaceComposer.cpp
@@ -206,7 +206,6 @@ status_t BnSurfaceComposer::onTransact(
             count = 0;
             SAFE_PARCEL_READ_SIZE(data.readUint32, &count, data.dataSize());
             std::vector<gui::EarlyWakeupInfo> earlyWakeupInfos;
-            state.setCapacity(count);
             for (size_t i = 0; i < count; i++) {
                 gui::EarlyWakeupInfo e;
                 e.readFromParcel(&data);
