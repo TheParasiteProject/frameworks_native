23622cc02781e21233f194bac81f81e3b37042fb
Surbhi Kadam <surbhikadam@google.com>
2025-05-21 11:34:49 -0700

    Avoid multiple allocations when pushing to the earlyWakeupInfos vector
    Bug: 419088315
    Flag: EXEMPT; refactor
    Test: presubmit
    Change-Id: I2d4d4cbee7ae74219b4d1705074772e74e5330f1

--- libs/gui/ISurfaceComposer.cpp
+++ libs/gui/ISurfaceComposer.cpp
@@ -206,6 +206,7 @@ status_t BnSurfaceComposer::onTransact(
             count = 0;
             SAFE_PARCEL_READ_SIZE(data.readUint32, &count, data.dataSize());
             std::vector<gui::EarlyWakeupInfo> earlyWakeupInfos;
+            earlyWakeupInfos.reserve(count);
             for (size_t i = 0; i < count; i++) {
                 gui::EarlyWakeupInfo e;
                 e.readFromParcel(&data);
