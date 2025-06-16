4d1ac0390c1ab851fa3ca411489e628594ec9c43
Patrick Williams <pdwilliams@google.com>
2025-06-16 15:34:26 -0500

    Implement ScreenCapture system API
    Bug: 370855681
    Flag: com.android.graphics.surfaceflinger.flags.readback_screenshot
    Test: atest CtsWindowManagerDeviceOther:android.server.wm.other.ScreenCaptureTest
    Change-Id: I59cd630cd179964e00e14aa5fb3981db6a66b91c

--- services/surfaceflinger/tests/unittests/TestableSurfaceFlinger.h
+++ services/surfaceflinger/tests/unittests/TestableSurfaceFlinger.h
@@ -473,7 +473,7 @@ public:
                           ui::Dataspace dataspace,
                           std::vector<std::pair<Layer*, sp<LayerFE>>>& layers,
                           const std::shared_ptr<renderengine::ExternalTexture>& buffer,
-                          bool disableBlur, bool isSecure, bool seamlessTransition) {
+                          bool disableBlur, bool isSecure, bool preserveDisplayColors) {
         Mutex::Autolock lock(mFlinger->mStateLock);
         ftl::FakeGuard guard(kMainThreadContext);
 
@@ -488,7 +488,8 @@ public:
                                                       .disableBlur = disableBlur,
                                                       .isGrayscale = false,
                                                       .isSecure = isSecure,
-                                                      .seamlessTransition = seamlessTransition,
+                                                      .preserveDisplayColors =
+                                                              preserveDisplayColors,
                                                       .displayBrightnessNits =
                                                               state.displayBrightnessNits,
                                                       .sdrWhitePointNits = state.sdrWhitePointNits,
