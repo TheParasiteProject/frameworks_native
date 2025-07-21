0cd49cd18492e0d7ca8e21df02c084f4b5b5b8c1
Alec Mouri <alecmouri@google.com>
2025-07-21 14:12:19 +0000

    Add frontend_caching_v0 flag
    This flag will be used to enable caching of frontend layer states.
    
    Bug: 433229921
    Flag: com.android.graphics.surfaceflinger.flags.frontend_caching_v0
    Test: Builds
    Change-Id: I4fed13a3ab60e8545386ef2a3d8d7e866434e146

--- services/surfaceflinger/common/include/common/FlagManager.h
+++ services/surfaceflinger/common/include/common/FlagManager.h
@@ -61,6 +61,7 @@ public:
     bool connected_displays_cursor() const;
     bool correct_virtual_display_power_state() const;
     bool disable_transparent_region_hint() const;
+    bool frontend_caching_v0() const;
     bool graphite_renderengine_preview_rollout() const;
     bool increase_missed_frame_jank_threshold() const;
     bool monitor_buffer_fences() const;
